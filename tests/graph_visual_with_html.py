0475a4de79bab35372783921d5194228d175c7f8
llp45135 <llp45135@gmail.com>
2025-02-20 10:42:35 +0800

    KG

--- tests/graph_visual_with_html.py
+++ tests/graph_visual_with_html.py
@@ -8,7 +8,7 @@ from pyvis.network import Network
 import random
 
 # Load the GraphML file
-G = nx.read_graphml("./tests/dickens/graph_chunk_entity_relation.graphml")
+G = nx.read_graphml("./tests/KG/graph_chunk_entity_relation.graphml")
 
 # Create a Pyvis network
 net = Network(height="100vh", notebook=True)
